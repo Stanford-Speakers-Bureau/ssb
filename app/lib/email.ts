2ae60e09597edf4506695e35bbff71b08030bd16
Michael Yu <64374317+amichaelyu@users.noreply.github.com>
2026-01-07 13:44:51 -0800

    email formatting

--- app/lib/email.ts
+++ app/lib/email.ts
@@ -534,7 +534,9 @@ async function generateTicketEmailHTML(
               isVIP
                 ? `
             <p style="margin: 0 0 16px 0; color: #f4f4f5; font-size: 16px; line-height: 1.6;">
-              We've reserved a seat for you in the front few rows. When you arrive at the event, please use the VIP entrance.
+              When you arrive at the event, please use the VIP entrance.
+              <br>
+              We've reserved a seat for you in the front few rows.
             </p>
             `
                 : ""
