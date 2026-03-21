3592c47458a185e53fef7d012d46c1af7eb80a51
Anish <22670462+anishanne@users.noreply.github.com>
2026-03-21 13:51:23 -0400

    better light theme colors

--- app/events/[eventID]/ticket-states/EventPassed.tsx
+++ app/events/[eventID]/ticket-states/EventPassed.tsx
@@ -6,7 +6,7 @@ export default function EventPassed() {
   return (
     <div>
       <div className="flex min-h-[3rem] items-center justify-center">
-        <p className="text-xs sm:text-sm text-yellow-400/80 text-center">
+        <p className="text-xs sm:text-sm text-zinc-600 dark:text-zinc-400 text-center">
           {TICKET_MESSAGES.EVENT_PASSED}
         </p>
       </div>
